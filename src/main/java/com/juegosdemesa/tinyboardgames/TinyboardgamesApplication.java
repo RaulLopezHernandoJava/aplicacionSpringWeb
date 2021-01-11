e415d0136b4bb9d0e03070b5240302319726fa4d
raullopezhernandojava <raullopezhernando@gmail.com>
2021-01-11 19:20:22 +0100

    Commit Inicial

---
+++ src/main/java/com/juegosdemesa/tinyboardgames/TinyboardgamesApplication.java
@@ -0,0 +1,13 @@
+package com.juegosdemesa.tinyboardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyboardgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyboardgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juegosdemesa/tinyboardgames/TinyboardgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juegosdemesa.tinyboardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyboardgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
